73a5da9169dd0d0f1dbad8857b14d2c2b17c8ec4
vivek12012003 <ritikverma1234567@gmail.com>
2026-05-20 13:15:20 +0530

    first commit

--- js/gallery.js
+++ js/gallery.js
@@ -75,7 +75,16 @@ const galleryAlbums = {
 
   "Videos": [
     "1.mp4",
-    "2.mp4"
+    "2.mp4",
+    "3.mp4",
+    "4.mp4",
+    "5.mp4",
+    "6.mp4",
+    "7.mp4",
+    "8.mp4",
+    "9.mp4",
+    "10.mp4",
+    "11.mp4"
   ],
 
 
